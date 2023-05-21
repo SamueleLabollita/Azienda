c702bd28d1d9b6123a3661bfc3bdb11b478a2f44
LabollitaS <101709291+SamueleLabollita@users.noreply.github.com>
2023-05-21 10:43:43 +0000

    salvato

--- demo/src/main/java/it/itispaleocapa/labollitas/App.java
+++ demo/src/main/java/it/itispaleocapa/labollitas/App.java
@@ -3,15 +3,122 @@ package it.itispaleocapa.labollitas;
 /**
  * Hello world!
  */
-public final class App {
-    private App() {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+class Personale {
+    private String id;
+    private String cognome;
+    private String nome;
+    private int annoAssunzione;
+
+    public Personale(String id, String cognome, String nome, int annoAssunzione) {
+        this.id = id;
+        this.cognome = cognome;
+        this.nome = nome;
+        this.annoAssunzione = annoAssunzione;
     }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public String getId() {
+        return id;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnnoAssunzione() {
+        return annoAssunzione;
     }
 }
+
+class Tecnico extends Personale {
+    private String areaCompetenza;
+    private boolean interno;
+
+    public Tecnico(String id, String cognome, String nome, int annoAssunzione, String areaCompetenza, boolean interno) {
+        super(id, cognome, nome, annoAssunzione);
+        this.areaCompetenza = areaCompetenza;
+        this.interno = interno;
+    }
+
+    public String getAreaCompetenza() {
+        return areaCompetenza;
+    }
+
+    public boolean isInterno() {
+        return interno;
+    }
+}
+
+class Funzionario extends Personale {
+    private int anniEsperienza;
+
+    public Funzionario(String id, String cognome, String nome, int annoAssunzione, int anniEsperienza) {
+        super(id, cognome, nome, annoAssunzione);
+        this.anniEsperienza = anniEsperienza;
+    }
+
+    public int getAnniEsperienza() {
+        return anniEsperienza;
+    }
+}
+
+class Dirigente extends Personale {
+    public Dirigente(String id, String cognome, String nome, int annoAssunzione) {
+        super(id, cognome, nome, annoAssunzione);
+    }
+}
+
+class Progetto {
+    private List<Personale> personale;
+
+    public Progetto() {
+        personale = new ArrayList<>();
+    }
+
+    public void aggiungiPersonale(Personale membro) throws personaleNulloException{
+        if (membro == null) {
+            throw personaleNulloException;
+        }
+        personale.add(membro);
+    }
+
+    public double getCostoComplessivo() {
+        double costoComplessivo = personale.stream()
+                .mapToDouble(this::calcolaCostoOrario)
+                .sum();
+
+        return costoComplessivo;
+    }
+
+    public double calcolaCostoOrario(Personale membro) throws personaleNulloException{
+        if (membro instanceof Tecnico) {
+            Tecnico tecnico = (Tecnico) membro;
+            double costoBase = tecnico.getAreaCompetenza().equals("informatica-telecomunicazioni") ? 40 : 50; //CONDIZIONE VERA : CONDIZIONE FALSA (TEC INF : TEC ELE)
+            double costoOrario = costoBase;
+            if (tecnico.isInterno()) {
+                int anniTrascorsi = 2023 - tecnico.getAnnoAssunzione();
+                costoOrario += anniTrascorsi;
+            }
+            return costoOrario;
+        } else if (membro instanceof Funzionario) {
+            Funzionario funzionario = (Funzionario) membro;
+            double costoOrario = funzionario.getAnniEsperienza() < 10 ? 70 : 80;
+            return costoOrario;
+        } else if (membro instanceof Dirigente) {
+            return 100;
+        } else 
+            throw personaleNulloException; 
+    }
+}
+class personaleNulloException extends Exception{
+
+}
+
+
--- demo/src/test/java/it/itispaleocapa/labollitas/AppTest.java
+++ demo/src/test/java/it/itispaleocapa/labollitas/AppTest.java
@@ -7,12 +7,94 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * Unit test for simple App.
  */
-class AppTest {
-    /**
-     * Rigorous Test.
-     */
+class ProgettoTest {
+
+    private Progetto progetto;
+
+    @BeforeEach
+    void setUp() {
+        progetto = new Progetto();
+    }
+
     @Test
-    void testApp() {
-        assertEquals(1, 1);
+    void aggiungiPersonaleSingoloMembro() {
+        Personale membro = new Tecnico("T001", "Labollita", "Samuele", 2018, "informatica-telecomunicazioni", true);
+        progetto.aggiungiPersonale(membro);
+        List<Personale> personale = progetto.getPersonale();
+        assertEquals(1, personale.size());
+        assertTrue(personale.contains(membro));
+    }
+
+    @Test
+    void aggiungiPersonaleNullMembro() {
+        assertThrows(IllegalArgumentException.class, () -> progetto.aggiungiPersonale(null));
+    }
+
+    @Test
+    void getCostoComplessivoNoPersonale() {
+        double costoComplessivo = progetto.getCostoComplessivo();
+        assertEquals(0, costoComplessivo);
+    }
+
+    @Test
+    void getCostoComplessivoSingleMember() {
+        Personale membro = new Tecnico("T001", "Labollita", "Samuele", 2018, "informatica-telecomunicazioni", true);
+        progetto.aggiungiPersonale(membro);
+        double costoComplessivo = progetto.getCostoComplessivo();
+        double expectedCosto = 40 + (2023 - 2018);
+        assertEquals(expectedCosto, costoComplessivo);
+    }
+
+    @Test
+    void getCostoComplessivoMultipleMembers() {
+        Personale tecnico1 = new Tecnico("T001", "Labollita", "Samuele", 2018, "informatica-telecomunicazioni", true);
+        Personale tecnico2 = new Tecnico("T002", "Verdi", "Luca", 2019, "elettronica-automazione", false);
+        Personale funzionario1 = new Funzionario("F001", "Bianchi", "Anna", 2015, 8);
+        progetto.aggiungiPersonale(tecnico1);
+        progetto.aggiungiPersonale(tecnico2);
+        progetto.aggiungiPersonale(funzionario1);
+        double costoComplessivo = progetto.getCostoComplessivo();
+        double expectedCosto = 40 + (2023 - 2018) + 50 + 70;
+        assertEquals(expectedCosto, costoComplessivo);
+    }
+
+    @Test
+    void calcolaCostoOrarioTecnicoInformaticaInterno() {
+        Tecnico tecnico = new Tecnico("T001", "Labollita", "Samuele", 2018, "informatica-telecomunicazioni", true);
+        double costoOrario = progetto.calcolaCostoOrario(tecnico);
+        double expectedCosto = 40 + (2023 - 2018);
+        assertEquals(expectedCosto, costoOrario);
+    }
+
+    @Test
+    void calcolaCostoOrarioTecnicoElettronicaEsterno() {
+        Tecnico tecnico = new Tecnico("T001", "Labollita", "Samuele", 2019, "elettronica-automazione", false);
+        double costoOrario = progetto.calcolaCostoOrario(tecnico);
+        double expectedCosto = 50;
+        assertEquals(expectedCosto, costoOrario);
+    }
+
+    @Test
+    void calcolaCostoOrarioFunzionarioJunior() {
+        Funzionario funzionario = new Funzionario("F001", "Bianchi", "Anna", 2015, 7);
+        double costoOrario = progetto.calcolaCostoOrario(funzionario);
+        double expectedCosto = 70;
+        assertEquals(expectedCosto, costoOrario);
+    }
+
+    @Test
+    void calcolaCostoOrarioFunzionarioSenior() {
+        Funzionario funzionario = new Funzionario("F001", "Bianchi", "Anna", 2008, 12);
+        double costoOrario = progetto.calcolaCostoOrario(funzionario);
+        double expectedCosto = 80;
+        assertEquals(expectedCosto, costoOrario);
+    }
+
+    @Test
+    void calcolaCostoOrarioDirigente() {
+        Dirigente dirigente = new Dirigente("D001", "Gialli", "Giuseppe", 2005);
+        double costoOrario = progetto.calcolaCostoOrario(dirigente);
+        double expectedCosto = 100;
+        assertEquals(expectedCosto, costoOrario);
     }
 }
